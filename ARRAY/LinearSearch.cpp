a3107ba52e35da536f7074429ff63e99db02f096
Shubhankar2003 <shubhankar733143@gmail.com>
2025-08-17 21:10:33 +0530

    initial commit

--- ARRAY/LinearSearch.cpp
+++ ARRAY/LinearSearch.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int LinearSearch(int arr[],int size,int target){
     for(int i = 0; i < size; i++){
-        if(arr[i] == target){
+        if(arr[i] == target){   // FOUND
             return i;
         }
     }
-    return -1;
+    return -1;  //NOT FOUND
 }
 
 int main(){
     int arr[] = {4,2,7,8,1,2,5};
     int size = sizeof(arr)/sizeof(int);
-    int result = LinearSearch(arr,size,8);
+    int target = 1;
+    int result = LinearSearch(arr,size,target);
     cout<<"The target index is : "<<result<<endl;
     return 0;
 }
